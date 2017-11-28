285629234a116b14ca95cb011ac6289298161ba2
TimFenwick15 <tjf20@hotmail.co.uk>
2017-11-28 20:10:52 +0000

    second

--- robot.py
+++ robot.py
@@ -1,10 +1,16 @@
 import rg
 class Robot:
+  def getNeighbourSquares(loc):
+    return [
+      (loc[0], loc[1]),
+
+    ]
   def act(self, game):
+    # If we're on a spawn square, get out
     if rg.loc_types(self.loc) != 'normal':
       return ['move', rg.toward(self.location, rg.CENTER_POINT)]
 
-    # if there are enemies around, attack them
+    # get list of allies and enemies
     allies = []
     enemies = []
     for loc, bot in game.robots.iteritems():
@@ -13,13 +19,23 @@ class Robot:
           allies.append(bot.location)
         else:
           enemies.append(bot.location)
+
+    # suicide condition
+    allyNeighbours = 0 # work out
+    enemyNeightbours = 0
+    if self.hp < 10 && allyNeighbours == 0 && (enemyNeightbours > 1 || enemyNeightbours == 1 && their hp > 10)
+      return ['suicide']
+
+    # attack near by enemies
+    for loc, bot in game.robots.iteritems():
+      if bot.player_id != self.player_id:
+        if rg.dist(loc, self.location) <= 1:
+          return ['attack', loc]
+
+    # move:
+    # - don't move into a square an ally could move into
+    # - move towards an ally
+    # - move towards an enemy
+
+    # guard
   
-
-        # if there are enemies around, attack them
-        for loc, bot in game.robots.iteritems():
-            if bot.player_id != self.player_id:
-                if rg.dist(loc, self.location) <= 1:
-                    return ['attack', loc]
-
-        # move toward the center
-        return ['move', rg.toward(self.location, rg.CENTER_POINT)]
